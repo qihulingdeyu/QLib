f03d05baadd7afffa1089ae129d8ac6235471088
qihulingdeyu <781157276@qq.com>
2015-08-31 18:09:32 +0800

    add class

---
+++ src/main/java/com/qing/qlib/CaughtApplication.java
@@ -0,0 +1,22 @@
+package com.qing.qlib;
+
+import android.app.Application;
+import android.util.Log;
+
+/**
+ * Created by zwq on 2015/08/24 10:37.<br/>
+ * 捕获异常信息
+ */
+public class CaughtApplication extends Application {
+
+    private static final String TAG = CaughtApplication.class.getName();
+    private CaughtExceptionHandler mCaughtExceptionHandler;
+
+    @Override
+    public void onCreate() {
+        Log.i(TAG, "--onCreate--");
+        mCaughtExceptionHandler = new CaughtExceptionHandler();
+        mCaughtExceptionHandler.Init(getApplicationContext());
+        super.onCreate();
+    }
+}
