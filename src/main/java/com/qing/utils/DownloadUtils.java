28b89fe787b2595ff5a61b16dd1e58888be69374
qihulingdeyu <781157276@qq.com>
2015-09-22 15:12:20 +0800

    modify...

--- src/main/java/com/qing/utils/DownloadUtils.java
+++ src/main/java/com/qing/utils/DownloadUtils.java
@@ -24,104 +24,110 @@ public class DownloadUtils {
         download(url, path, null, callback);
     }
 
-    public static void download(String url, String path, String fileName, DownloadCallback callback){
-        if(url == null || path==null){
-            if (callback!=null) callback.postResult(DownloadCallback.FAIL, "下载地址或保存目录不能为空");
-        }
-        boolean isFile = false;
-        File file = new File(path);
-        if (file.isFile()){
-            file = file.getParentFile();
-            isFile = true;
-        }else{
-            if (StringUtils.isNullOrEmpty(fileName)) {
-                if (callback!=null) callback.postResult(DownloadCallback.FAIL, "文件名不能为空");
-            }
-        }
-        if(!file.exists()){
-            if(!file.mkdirs()){
-                if (callback!=null) callback.postResult(DownloadCallback.FAIL, "目录创建失败");
-            }
-        }
-        if (isFile) {
-            file = new File(path);
-        }else{
-            file = new File(path, fileName);
-        }
-
-        if(file.exists()) {
-            if (callback!=null) callback.postResult(DownloadCallback.SUCCESS);
-        }else{
-            try {
-                if(!file.createNewFile()){
-                    if (callback!=null) callback.postResult(DownloadCallback.FAIL, "文件创建失败");
+    public static void download(final String url, final String path, final String fileName, final DownloadCallback callback){
+        ThreadUtils mThread = new ThreadUtils() {
+            @Override
+            public void execute() {
+                if(url == null || path==null){
+                    if (callback!=null) callback.postResult(DownloadCallback.FAIL, "下载地址或保存目录不能为空");
                 }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        long fileSize = file.length();
-        int contentSize = 0;
-
-        InputStream inputStream = null;
-        ByteArrayOutputStream byteStream = null;
-        FileOutputStream fileOutput = null;
-        try {
-            URL _url = new URL(url);
-            HttpURLConnection conn = (HttpURLConnection)_url.openConnection();
-            conn.setRequestMethod("GET");
-            conn.setConnectTimeout(40*1000);
-            conn.setReadTimeout(90*1000);
-            conn.setDoInput(true);
-            conn.connect();
-
-            if (conn.getResponseCode() == 200) {
-                if (callback!=null) callback.postResult(DownloadCallback.START);
-                contentSize = conn.getContentLength();
-
-                if(fileSize != contentSize) {
-                    inputStream = conn.getInputStream();
-                    byteStream = new ByteArrayOutputStream();
-
-                    int size = 0;
-                    int len = 0;
-                    byte buffer[] = new byte[4096];
-                    fileOutput = new FileOutputStream(file.getAbsolutePath());
-
-                    while((len = inputStream.read(buffer, 0, buffer.length)) != -1) {
-                        fileOutput.write(buffer, 0, len);
-                        if (callback!=null) {
-                            size += len;
-                            callback.postResult(DownloadCallback.LOADING, size/contentSize);
-                        }
+                boolean isFile = false;
+                File file = new File(path);
+                if (file.isFile()){
+                    file = file.getParentFile();
+                    isFile = true;
+                }else{
+                    if (StringUtils.isNullOrEmpty(fileName)) {
+                        if (callback!=null) callback.postResult(DownloadCallback.FAIL, "文件名不能为空");
                     }
                 }
-                if (callback!=null) callback.postResult(DownloadCallback.SUCCESS);
-            }else{
-                if (callback!=null) callback.postResult(DownloadCallback.FAIL, "下载失败:"+conn.getResponseMessage());
+                if(!file.exists()){
+                    if(!file.mkdirs()){
+                        if (callback!=null) callback.postResult(DownloadCallback.FAIL, "目录创建失败");
+                    }
+                }
+                if (isFile) {
+                    file = new File(path);
+                }else{
+                    file = new File(path, fileName);
+                }
+
+                if(file.exists()) {
+                    if (callback!=null) callback.postResult(DownloadCallback.SUCCESS);
+                }else{
+                    try {
+                        if(!file.createNewFile()){
+                            if (callback!=null) callback.postResult(DownloadCallback.FAIL, "文件创建失败");
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                final long fileSize = file.length();
+                int contentSize = 0;
+
+                InputStream inputStream = null;
+                ByteArrayOutputStream byteStream = null;
+                FileOutputStream fileOutput = null;
+                try {
+                    URL _url = new URL(url);
+                    HttpURLConnection conn = (HttpURLConnection)_url.openConnection();
+                    conn.setRequestMethod("GET");
+                    conn.setConnectTimeout(40*1000);
+                    conn.setReadTimeout(90*1000);
+                    conn.setDoInput(true);
+                    conn.connect();
+
+                    if (conn.getResponseCode() == 200) {
+                        if (callback!=null) callback.postResult(DownloadCallback.START);
+                        contentSize = conn.getContentLength();
+
+                        if(fileSize != contentSize) {
+                            inputStream = conn.getInputStream();
+                            byteStream = new ByteArrayOutputStream();
+
+                            int size = 0;
+                            int len = 0;
+                            byte buffer[] = new byte[4096];
+                            fileOutput = new FileOutputStream(file.getAbsolutePath());
+
+                            while((len = inputStream.read(buffer, 0, buffer.length)) != -1) {
+                                fileOutput.write(buffer, 0, len);
+                                if (callback!=null) {
+                                    size += len;
+                                    callback.postResult(DownloadCallback.LOADING, size/contentSize);
+                                }
+                            }
+                        }
+                        if (callback!=null) callback.postResult(DownloadCallback.SUCCESS);
+                    }else{
+                        if (callback!=null) callback.postResult(DownloadCallback.FAIL, "下载失败:"+conn.getResponseMessage());
+                    }
+                } catch (Exception e){
+                    e.printStackTrace();
+                    if (callback!=null) callback.postResult(DownloadCallback.FAIL, "下载失败:"+e.getMessage());
+                } finally {
+                    try {
+                        if (fileOutput!=null) {
+                            fileOutput.close();
+                            fileOutput = null;
+                        }
+                        if (byteStream!=null){
+                            byteStream.close();
+                            byteStream = null;
+                        }
+                        if (inputStream!=null){
+                            inputStream.close();
+                            inputStream = null;
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                if (callback!=null) callback.postResult(DownloadCallback.FINISH);
             }
-        } catch (Exception e){
-            e.printStackTrace();
-            if (callback!=null) callback.postResult(DownloadCallback.FAIL, "下载失败:"+e.getMessage());
-        } finally {
-            try {
-                if (fileOutput!=null) {
-                    fileOutput.close();
-                    fileOutput = null;
-                }
-                if (byteStream!=null){
-                    byteStream.close();
-                    byteStream = null;
-                }
-                if (inputStream!=null){
-                    inputStream.close();
-                    inputStream = null;
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        if (callback!=null) callback.postResult(DownloadCallback.FINISH);
+        };
+        mThread.start();
     }
 
 }
